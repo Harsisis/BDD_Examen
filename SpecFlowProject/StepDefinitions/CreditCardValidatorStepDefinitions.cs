47a8d6a5836abf8670cf8163c147d1ab0205e7cb
Haorsis <cadetgauthier3@gmail.com>
2022-11-10 16:47:48 +0100

    remove unused imports

--- SpecFlowProject/StepDefinitions/CreditCardValidatorStepDefinitions.cs
+++ SpecFlowProject/StepDefinitions/CreditCardValidatorStepDefinitions.cs
@@ -1,15 +1,10 @@
 using MStest_Workstate1.Helpers;
 using OpenQA.Selenium;
-using SpecFlowProject_BDD.BasesClasses;
-using System;
 using System.Text.RegularExpressions;
-using TechTalk.SpecFlow;
 
-namespace SpecFlowProject.StepDefinitions
-{
+namespace SpecFlowProject.StepDefinitions {
     [Binding]
-    public class CreditCardValidatorStepDefinitions
-    {
+    public class CreditCardValidatorStepDefinitions {
 
         private By creditCardNumber = By.Id("creditCardNumber");
         private By expirationDate = By.Id("expirationDate");
@@ -18,8 +13,7 @@ namespace SpecFlowProject.StepDefinitions
         private By paymentConfirmed = By.Id("paymentConfirmed");
 
         [Given(@"user fills the three inputs")]
-        public void GivenUserFillsTheThreeInputs()
-        {
+        public void GivenUserFillsTheThreeInputs() {
             TextBoxHelper.TypeInTextBox(creditCardNumber, "1234567890123456");
             TextBoxHelper.TypeInTextBox(expirationDate, "09/2001");
             TextBoxHelper.TypeInTextBox(cvc, "123");
@@ -48,61 +42,52 @@ namespace SpecFlowProject.StepDefinitions
 
 
         [Given(@"credit card number is sixteen digits long")]
-        public void GivenCreditCardNumberIsSixteenDigitsLong()
-        {
+        public void GivenCreditCardNumberIsSixteenDigitsLong() {
             WebElement cardNumber = (WebElement)GenericHelper.GetElement(creditCardNumber);
             Assert.IsTrue(cardNumber.GetAttribute("value").Length == 16);
         }
 
         [Given(@"expiration date is at format MM/YYYY")]
-        public void GivenExpirationDateIsAtFormatMMYYYY()
-        {
+        public void GivenExpirationDateIsAtFormatMMYYYY() {
             WebElement expirationDateNumber = (WebElement)GenericHelper.GetElement(expirationDate);
             Assert.IsTrue(Regex.IsMatch(expirationDateNumber.GetAttribute("value"), "[0-9][0-9]/[0-9][0-9][0-9][0-9]"));
         }
 
         [Given(@"cvc is three digits long")]
-        public void GivenCvcIsThreeDigitsLong()
-        {
+        public void GivenCvcIsThreeDigitsLong() {
             WebElement cvcNumber = (WebElement)GenericHelper.GetElement(cvc);
             Assert.IsTrue(cvcNumber.GetAttribute("value").Length == 3);
         }
 
         [When(@"submit button is pressed")]
-        public void WhenSubmitButtonIsPressed()
-        {
+        public void WhenSubmitButtonIsPressed() {
             ButtonHelper.ClickButton(submit);
         }
 
         [Then(@"user is on page paymentConfirmed")]
-        public void ThenUserIsOnPagePaymentConfirmed()
-        {
+        public void ThenUserIsOnPagePaymentConfirmed() {
             Assert.IsTrue(GenericHelper.IsElementPresentOnce(paymentConfirmed));
         }
 
         [Given(@"credit card number is not sixteen digits long")]
-        public void GivenCreditCardNumberIsNotSixteenDigitsLong()
-        {
+        public void GivenCreditCardNumberIsNotSixteenDigitsLong() {
             WebElement cardNumber = (WebElement)GenericHelper.GetElement(creditCardNumber);
             Assert.IsTrue(cardNumber.GetAttribute("value").Length != 16);
         }
 
         [Then(@"user is on homePage")]
-        public void ThenUserIsOnHomePage()
-        {
+        public void ThenUserIsOnHomePage() {
             Assert.IsFalse(GenericHelper.IsElementPresentOnce(paymentConfirmed));
         }
 
         [Given(@"expiration date is not at format MM/YYYY")]
-        public void GivenExpirationDateIsNotAtFormatMMYYYY()
-        {
+        public void GivenExpirationDateIsNotAtFormatMMYYYY() {
             WebElement expirationDateNumber = (WebElement)GenericHelper.GetElement(expirationDate);
             Assert.IsFalse(Regex.IsMatch(expirationDateNumber.GetAttribute("value"), "[0-9][0-9]/[0-9][0-9][0-9][0-9]"));
         }
 
         [Given(@"cvc is not three digits long")]
-        public void GivenCvcIsNotThreeDigitsLong()
-        {
+        public void GivenCvcIsNotThreeDigitsLong() {
             WebElement cvcNumber = (WebElement)GenericHelper.GetElement(cvc);
             Assert.IsTrue(cvcNumber.GetAttribute("value").Length != 3);
         }
